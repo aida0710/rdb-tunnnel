269d363621c51624384ba543d46bdbad26669653
aida0710 <51500566+aida0710@users.noreply.github.com>
2024-10-15 09:29:28 +0900

    型の追加と可視性変更に伴う修正

--- src/packet_analysis.rs
+++ src/packet_analysis.rs
@@ -1,16 +1,18 @@
 use pcap::{Active, Capture};
 use std::collections::HashMap;
 use std::time::Duration;
-use crate::packet_reassembly::ip_reassembly::IpReassembler;
-use crate::packet_reassembly::packet_processor::process_packet;
-use crate::packet_reassembly::tcp_stream;
-use crate::packet_reassembly::tcp_stream::{TcpStream, TcpStreamKey};
+use crate::host_ids::{process_packet, IpReassembler, TcpStream, TcpStreamKey};
+use crate::host_ids::TcpState;
 
 pub fn packet_analysis(mut cap: Capture<Active>) -> Result<(), Box<dyn std::error::Error>> {
     let mut streams: HashMap<TcpStreamKey, TcpStream> = HashMap::new();
-    let mut ip_reassembler = IpReassembler::new(Duration::from_secs(30));
+    let mut ip_reassembler: IpReassembler = IpReassembler::new(Duration::from_secs(30));
 
     while let Ok(packet) = cap.next_packet() {
+        // VPN処理
+
+
+        // IDS処理
         match process_packet(&packet, &mut streams, &mut ip_reassembler) {
             Ok(_) => (),
             Err(e) => eprintln!("パケット処理中にエラーが発生しました: {}", e),
@@ -18,7 +20,7 @@ pub fn packet_analysis(mut cap: Capture<Active>) -> Result<(), Box<dyn std::erro
 
         // 古いストリームの削除
         streams.retain(|_, stream| {
-            stream.last_activity.elapsed() < Duration::from_secs(300) || stream.state != tcp_stream::TcpState::Closed
+            stream.last_activity.elapsed() < Duration::from_secs(300) || stream.state != TcpState::Closed
         });
     }
 
