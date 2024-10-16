e020a1a40c2adcb11c321bb37c5c46522090fb47
相田 優希 <51500566+aida0710@users.noreply.github.com>
2024-10-16 13:47:35 +0900

    src/dstを分けて扱うように修正

--- src/vpn/firewall_packet.rs
+++ src/vpn/firewall_packet.rs
@@ -1,18 +1,23 @@
-use std::net::IpAddr;
 use pnet::packet::ip::IpNextHeaderProtocol;
+use std::net::IpAddr;
 
+#[derive(Clone, Copy)]
 pub struct FirewallPacket {
-    pub ip_address: IpAddr,
-    pub port: u16,
+    pub src_ip: IpAddr,
+    pub dst_ip: IpAddr,
+    pub src_port: u16,
+    pub dst_port: u16,
     pub ip_version: u8,
     pub next_header_protocol: IpNextHeaderProtocol,
 }
 
 impl FirewallPacket {
-    pub fn new(ip_address: IpAddr, port: u16, ip_version: u8, next_header_protocol: IpNextHeaderProtocol) -> Self {
+    pub fn new(src_ip: IpAddr, dst_ip: IpAddr, src_port: u16, dst_port: u16, ip_version: u8, next_header_protocol: IpNextHeaderProtocol) -> FirewallPacket {
         FirewallPacket {
-            ip_address,
-            port,
+            src_ip,
+            dst_ip,
+            src_port,
+            dst_port,
             ip_version,
             next_header_protocol,
         }
