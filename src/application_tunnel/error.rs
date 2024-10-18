201fbaef82f7a506c83aefe66c470bc89a892eeb
aida0710 <51500566+aida0710@users.noreply.github.com>
2024-10-18 10:14:08 +0900

    errorを追加

---
+++ src/application_tunnel/error.rs
@@ -0,0 +1,9 @@
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum VpnError {
+    // #[error("デバイスの選択に失敗しました: {0}")]
+    // DeviceSelectionError(String),
+
+
+}
---
+++ src/host_idps/error.rs
@@ -0,0 +1,9 @@
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum HostIDPSError {
+    // #[error("デバイスの選択に失敗しました: {0}")]
+    // DeviceSelectionError(String),
+
+
+}
