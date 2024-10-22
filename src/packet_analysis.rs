4792025ace15b338b59248ab321466fc89c56fe9
aida0710 <51500566+aida0710@users.noreply.github.com>
2024-10-22 17:36:52 +0900

    順番を修正

--- src/packet_analysis.rs
+++ src/packet_analysis.rs
@@ -2,9 +2,8 @@ use crate::host_idps::{process_packet, IpReassembler, TcpState};
 use pnet::datalink::Channel::Ethernet;
 use pnet::datalink::{self, NetworkInterface};
 use std::time::Duration;
-use crate::rdb_tunnel::rdb_tunnel;
 
-pub fn packet_analysis(interface: NetworkInterface) -> Result<(), Box<dyn std::error::Error>> {
+pub async fn packet_analysis(interface: NetworkInterface) -> Result<(), Box<dyn std::error::Error>> {
     let (_, mut rx) = match datalink::channel(&interface, Default::default()) {
         Ok(Ethernet(tx, rx)) => (tx, rx),
         Ok(_) => return Err("未対応のチャンネルタイプです".into()),
@@ -16,15 +15,15 @@ pub fn packet_analysis(interface: NetworkInterface) -> Result<(), Box<dyn std::e
 
     loop {
         match rx.next() {
-            Ok(packet) => {
-                // rdb_tunnel
-                match rdb_tunnel::rdb_tunnel(&packet) { _ => {} }
-
+            Ok(ethernet_packet) => {
                 // イーサネットフレームの解析
-                match process_packet(&packet, &mut streams, &mut ip_reassembler) {
+                match process_packet(&ethernet_packet, &mut streams, &mut ip_reassembler) {
                     Ok(_) => (),
                     Err(e) => eprintln!("パケット処理中にエラーが発生しました: {}", e),
                 }
+
+                // rdb_tunnel
+                crate::rdb_tunnel::rdb_tunnel_packet_write(&ethernet_packet).await?;
             }
             Err(e) => eprintln!("パケットの読み取り中にエラーが発生しました: {}", e),
         }
