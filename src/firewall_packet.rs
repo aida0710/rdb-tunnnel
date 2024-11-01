0e9dfe33ef8117a164d930e22bfa1da8c3b5cc19
aida0710 <51500566+aida0710@users.noreply.github.com>
2024-11-01 16:56:00 +0900

    `FirewallPacket`構造体に`Debug`トレイトを追加し、`new`関数を改行して可読性を向上。

--- src/firewall_packet.rs
+++ src/firewall_packet.rs
@@ -1,6 +1,6 @@
 use std::net::IpAddr;
 
-#[derive(Clone, Copy)]
+#[derive(Debug)]
 pub struct FirewallPacket {
     pub src_ip: IpAddr,
     pub dst_ip: IpAddr,
@@ -10,8 +10,14 @@ pub struct FirewallPacket {
 }
 
 impl FirewallPacket {
-    pub fn new(src_ip: IpAddr, dst_ip: IpAddr, src_port: u16, dst_port: u16, ip_version: u8) -> FirewallPacket {
-        FirewallPacket {
+    pub fn new(
+        src_ip: IpAddr,
+        dst_ip: IpAddr,
+        src_port: u16,
+        dst_port: u16,
+        ip_version: u8,
+    ) -> Self {
+        Self {
             src_ip,
             dst_ip,
             src_port,
